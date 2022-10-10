11ca0bdd255fea48970f24e175883a411d29c591
giadavolpin <volpingiada@gmail.com>
2022-10-10 15:19:29 +0200

    for

---
+++ script.js
@@ -0,0 +1,20 @@
+/* 
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per un altro? 
+Abbiamo visto qualcosa di particolare che possiamo usare?
+
+da 1 a 100 in console 
+multipli di 3 FIzz
+multipli di 5 Buzz
+multipli di 3 e 5 FizzBuzz
+*/
+
+
+const nome = document.getElementById('fizz');
+for(let i = 0; i < 100; i + 3 ) {
+     console.log(fizz);
+     
+ }
+/* for(let i = 0; i < 100; i + 5 ) {
+    console.log()   
+} */
